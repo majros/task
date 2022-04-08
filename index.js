a4014f111a07098c92598d8ad5d0d85cb2fb701d
Andrew <a.a.zgara@gmail.com>
2022-04-08 17:47:03 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const t = 174;
+const k = 3;
+const ls = [51, 56, 58, 59, 61];
+
+const chooseOptimalDistance = (t, k, ls) => {
+    if (t < 0) throw 't must be >= 0';
+    if (k < 1) throw 'K must be >= 1';
+    if (ls.length === 0) throw 'Array ls must contain 1 element';
+    ls.forEach((i) => {if (i < 1) throw `${i} element of array must be >= 0`});
+
+    const lsn = ls.length;
+    const sum = [];
+    for (let n = 0; n < lsn; n++) {
+        const a = ls.slice();
+        a.splice(n, 1);
+        console.log(n, a);
+        for (let i = 0; i < lsn-1; i++) {
+            const b = a.slice();
+            b.splice(i, 1);
+            sum.push(b.reduce((a,b) => {return(a+b)}));
+            console.log(`i=${i}, b=[${b}], sum=${sum}`)
+        }
+    }
+    console.log(sum.sort().filter((i, n) => !sum.includes(i, n + 1)));
+};
+
+
+try {
+    chooseOptimalDistance(t, k, ls)
+} catch (e) {
+        console.error(e)
+}
